fdad28b9c86d9440beed774e74ef74a287772fa9
Spinomik <spinomik@onet.pl>
2022-10-11 17:36:10 +0200

    19 - tworzenie starteru

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,42 @@
 import { Component } from '@angular/core';
+import { QUOTES } from './models/database';
+import { Quotation } from './models/quotation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+
+  showform: boolean;
+  quotes: Quotation[];
+  quotation: Quotation;
+
+  constructor() {
+    this.showform = false;
+    this.quotes = QUOTES;
+    this.quotation = {
+      author: '',
+      sentence: '',
+      votes: 0,
+    }
+  }
+
+  editModeOn() {
+    this.showform = !this.showform;
+  }
+
+  addQuotation() {
+    this.quotes.unshift(this.quotation);
+    this.quotation = {
+      author: '',
+      sentence: '',
+      votes: 0
+    }
+  }
+
+  addVote(quotation: Quotation, vote: number) {
+    quotation.votes += vote;
+  }
+}
