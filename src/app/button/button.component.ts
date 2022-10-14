5e23a2f993187eecc591809f4e62dd9bbaed8768
Spinomik <spinomik@onet.pl>
2022-10-14 19:12:23 +0200

    24 - Projekcja treści

---
+++ src/app/button/button.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter, Output, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+  @Output() clickButton = new EventEmitter<void>();
+  @Input() customClasses: string;
+
+  constructor() { }
+
+}
